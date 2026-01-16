efe9283c21f7836f959d204a9eb919a15ebad1f0
Mohammed Ali <mohammed.sayed16677@sakec.ac.in>
2026-01-16 20:52:29 +0530

    p

--- src/compi/Footer.jsx
+++ src/compi/Footer.jsx
@@ -4,8 +4,8 @@ export const Footer = () => {
         <div className="text-gray-200 ">
           <b>Contact me At &copy; 8828171231</b>
           <div className="hover:text-blue-400">Hey iam using Whatsapp at given Number</div>
-          <a className="hover:text-blue-400">https://github/786-sys.com</a>
-          <a className="hover:text-blue-400">https://www.linkedin.com/in/taha-sayyed-81b166368/</a>
+          <div className="hover:text-blue-400">https://github/786-sys.com</div>
+          <div className="hover:text-blue-400">https://www.linkedin.com/in/taha-sayyed-81b166368/</div>
         </div>
     </div>
   )
