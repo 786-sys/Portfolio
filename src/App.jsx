b8005aa714a9293690dc186a972b9cf645ad49f7
Mohammed Ali <mohammed.sayed16677@sakec.ac.in>
2026-01-16 20:37:30 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import ROUTER from './route/router'
+import ROUTER from './route/ROUTER'
 function App() {
   const router=createBrowserRouter(ROUTER())
   return (
