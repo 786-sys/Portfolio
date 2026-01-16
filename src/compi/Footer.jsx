d91194ee021027255a8d5d81354ef6b880522ec6
Mohammed Ali <mohammed.sayed16677@sakec.ac.in>
2026-01-16 21:12:48 +0530

    p

--- src/compi/Footer.jsx
+++ src/compi/Footer.jsx
@@ -7,25 +7,25 @@ export const Footer = () => {
           Hey iam using Whatsapp at given Number
         </div>
         {/* github */}
-        <a
+       <div className="flex flex-col justify-center item-center text-center">
+         <a
           href="https://github.com/786-sys/"
           target="_blank"
           rel="noopener noreferrer"
           aria-label="GitHub Profile"
         >
-          <i className="fa fa-github text-2xl text-gray-800 hover:text-black transition-transform duration-300 hover:scale-110"></i>
+          <i className="fa fa-github text-2xl text-white"></i>
         </a>
-        {/* linked in */}
-        <div>
-          <a
-            href="https://www.linkedin.com/in/taha-sayyed-81b166368/"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="LinkedIn Profile"
-          >
-            <i className="fa fa-linkedin text-2xl text-[#0A66C2] hover:text-blue-800 transition-transform duration-300 hover:scale-110"></i>{" "}
-          </a>
-        </div>
+
+        <a
+          href="https://www.linkedin.com/in/taha-sayyed-81b166368/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn Profile"
+        >
+          <i className="fa fa-linkedin text-2xl text-white-600"></i>
+        </a>
+       </div>
       </div>
     </div>
   );
